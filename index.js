1f3ed7621c77c48e1ef5b000c2abc79cd0f93e2f
sivagiri0203 <mastersivagiri@gmail.com>
2025-12-27 10:42:02 +0530

    second

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 connectDB();
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", authRoutes);
+app.get("/", (req, res) => {
+  res.status(200).send("User Authentication API is running 🚀");
+});
 
 app.listen(6000, () =>
   console.log("Server running at http://localhost:6000")
